9f6d8f66b3f075fc8f765b7ae35901a37fd09eac
Hjorthen <Sebastian@10.16.71.155>
2016-03-25 11:36:16 +0100

    Renamed VsCon to WeightServer and added support for adding port#

--- src/WeightServer.java
+++ src/WeightServer.java
@@ -5,15 +5,13 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class VsCon {
-	static double brutto=0;
-	static double tara=0;
-	static String inline;
-	static String indtDisp= "";
-	static int portdst = 8000;
-	static ConnectionHandler connection;
+public class WeightServer {
+	String inline;
+	String indtDisp= "";
+	ConnectionHandler connection;
+	static final int defaultPort = 8000;
 	static boolean rm20flag = false;
-	private static void connect(int port) throws IOException
+	private void connect(int port) throws IOException
 	{
 		connection = new ConnectionHandler(port);
 		System.out.println("Venter paa connection på port " + port );
@@ -21,23 +19,13 @@ public class VsCon {
 		System.out.println("paa kommando linien for andet portnr");
 		
 	}
-	private static void disconnect()
+	public void start()
 	{
-		System.out.println("");
-		System.out.println("Program stoppet Q modtaget paa com   port");
-		try {
-			System.in.close();
-			System.out.close();
-			connection.close();
-			System.exit(0);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		start(defaultPort);
 	}
-	public static void main(String[] args) throws IOException{
-		connect(portdst);
+	public void start(int port)
+	{
+		connect(port);
 		printmenu();
 		try{
 			String[] tokens = null;
@@ -87,53 +75,52 @@ public class VsCon {
 				}
 			}
 				System.out.println(String.join(",", tokens));
-//				if (inline.startsWith("RM")){                        
-//					// ikke implimenteret
-//
-//				}
-//				else if (inline.startsWith("D")){
-//					if (inline.equals("DW"))
-//						indtDisp="";
-//					else
-//						indtDisp=(inline.substring(2, inline.length()));//her skal anførselstegn udm.
-//					printmenu();
-//					outstream.writeBytes("DB"+"\r\n");
-//				}
-//				else if (inline.startsWith("T")){
-//					outstream.writeBytes("T S " + (tara) + " kg "+"\r\n");        //HVOR MANGE SPACE?
-//					tara=brutto;
-//					printmenu();
-//				}
-//				else if (inline.startsWith("S")){
-//					printmenu();
-//					outstream.writeBytes("S S " + (brutto-tara)+ " kg "  +"\r\n");//HVOR MANGE SPACE?
-//				}
-//				else if (inline.startsWith("B")){ //denne ordre findes ikke på en fysisk vægt
-//					String temp= inline.substring(2,inline.length());
-//					brutto = Double.parseDouble(temp);
-//					printmenu();
-//					outstream.writeBytes("DB"+"\r\n");
-//				}
-//				else if ((inline.startsWith("Q"))){
-//					
-//				}
-//
-//
-//				else { 
-//					printmenu();
-//					outstream.writeBytes("ES"+"\r\n");
-//				}
 			}
 		}
 		catch (Exception e){
 			System.out.println("Exception: "+e.getMessage());
 		}
 	}
-	public static void printmenu(){
+	private void disconnect()
+	{
+		System.out.println("");
+		System.out.println("Program stoppet Q modtaget paa com   port");
+		try {
+			System.in.close();
+			System.out.close();
+			connection.close();
+			System.exit(0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String[] args) throws IOException{
+		int port = defaultPort;
+		try
+		{
+			if(args.length>0)
+			{
+				port = Integer.parseUnsignedInt(args[0]);
+			}
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Specified port was not a number. Resetting to default.");
+		}
+		finally
+		{
+			WeightServer ws = new WeightServer();
+			ws.start(port);
+		}
+		
+	}
+	public void printmenu(){
 		if(true)
 			return;
 		for (int i=0;i<2;i++)
-			System.out.println("                                                 ");
+		System.out.println("                                                 ");
 		System.out.println("*************************************************");
 		System.out.println("Netto: " + (brutto-tara)+ " kg"                   );
 		System.out.println("Instruktionsdisplay: " +  indtDisp    );
