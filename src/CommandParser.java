350007e4260f2fc16e9f10767a65577186685281
Hjorthen <Sebastian@10.16.71.155>
2016-03-28 13:04:01 +0200

    Created command parser. This will split the command strings into tokens.

---
+++ src/CommandParser.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandParser {
+	private String command;
+	private int p = 0;
+	private boolean end = false;
+	public CommandParser(String s)
+	{
+		command = s;
+	}
+	//Todo: make it take double spaces into account
+	public String nextToken()
+	{
+		if(p>=command.length())
+		{
+			return null;
+		}
+		if(command.length()>p && command.charAt(p)=='\"')
+		{
+			int i = command.indexOf('\"', p+1);
+			if(i==-1)
+			{
+				return null;
+			}
+			String token = command.substring(p+1, i);
+			p = i+1;
+			return token;
+		}
+		else
+		{
+			int i = command.indexOf(' ', p);
+			if(i==-1)
+			{
+				return command.substring(p, command.length());
+			}
+			else
+			{
+				String token = command.substring(p, i);
+				p = i+1;
+				if(token.isEmpty())
+				{
+					return null;
+				}
+				else
+				{
+					return token;
+				}
+			}
+		}
+		
+		
+				
+	}
+	public String[] getTokens()
+	{
+		String s;
+		List<String> tokens = new ArrayList<String>();
+		while((s = nextToken())!=null)
+		{
+			tokens.add(s);
+		}
+		return (String[])tokens.toArray(new String[tokens.size()]);
+	}
+	public static void main(String[] args) {
+		String command = "Dette er en \"meget lang test\" 22 \"Trolololo\"";
+		System.out.println(command);
+		CommandParser p = new CommandParser(command);
+		for (String string : p.getTokens()) {
+			System.out.println(string);
+		}
+	}
+}
